28df718af56cc9b8917adcdb09c9d3752ede4d9a
Zhong Zhenyu <nczzy1997@gmail.com>
2024-12-06 19:52:20 +0800

    Output filtered generated predictions to the report directory

--- scripts/evaluation/detection/filter-detection-results.py
+++ scripts/evaluation/detection/filter-detection-results.py
@@ -16,22 +16,32 @@ black_list = [
 ]
 
 
-def main(path: str):
+def main(
+        path: str,
+        report_root: str = '../../../reports/evaluation/detection',
+):
     path = pathlib.Path(path)
+    if path.is_dir():
+        path /= 'generated_predictions.jsonl'
+    report_path = pathlib.Path(report_root) / path.parent.stem
+    report_path.mkdir(parents=True, exist_ok=True)
     data = csc.datasets.utils.load_data_from_file(path)
-    with path.with_suffix('.txt').open('w') as text_file, path.with_suffix('.filtered.jsonl').open('w') as jsonl_file:
+    with (
+        (report_path / 'filtered_output.txt').open('w') as filtered_output,
+        (report_path / 'filtered_generated_predictions.jsonl').open('w') as filtered_generated_predictions,
+    ):
         for item in data:
             label, predict = item['label'], item['predict']
             for char in black_list:
                 label = label.replace(f'<csc>{char}</csc>', char)
                 predict = predict.replace(f'<csc>{char}</csc>', char)
-            jsonl_file.write(json.dumps({
+            filtered_generated_predictions.write(json.dumps({
                 'prompt': item['prompt'],
                 'label': label,
                 'predict': predict,
             }, ensure_ascii=False) + '\n')
             if '<csc>' in predict:
-                text_file.write(f'{predict}\n')
+                filtered_output.write(f'{predict}\n')
 
 
 if __name__ == '__main__':
