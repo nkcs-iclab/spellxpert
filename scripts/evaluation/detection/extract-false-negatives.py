9e3c108328dc9d14fb258719c781e0b90d13c320
Zhong Zhenyu <nczzy1997@gmail.com>
2024-12-19 16:25:57 +0800

    Add script to extract false negatives

---
+++ scripts/evaluation/detection/extract-false-negatives.py
@@ -0,0 +1,28 @@
+import fire
+import json
+import pathlib
+
+import csc
+
+
+def main(
+        path: str,
+        template: int,
+        report_root: str = '../../../reports/evaluation/detection',
+):
+    path = pathlib.Path(path)
+    report_path = pathlib.Path(report_root) / path.parent.stem
+    report_path.mkdir(parents=True, exist_ok=True)
+    data = csc.datasets.utils.load_data_from_file(path)
+    report_path /= 'false-negatives.jsonl'
+    template = csc.evaluation.detection.templates[template]
+    with report_path.open('w') as f:
+        for item in data:
+            label, predict = item['label'], item['predict']
+            tp, fp, fn, label_array, predict_array = template.eval_one(label, predict)
+            if fn > 0:
+                f.write(json.dumps(item, ensure_ascii=False) + '\n')
+
+
+if __name__ == '__main__':
+    fire.Fire(main)
